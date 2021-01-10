a29743bb9feee0b8661f56bdcfa4d5f7ae499389
Sumit Banerjee <sum123.ban@gmail.com>
2021-01-10 05:41:57 +0530

    Post Storage Capability retained

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,16 +1,33 @@
 var postSelected;
 
+test = () => {
+    var author = postSelected.querySelector(".author p").innerText.trim();
+    var title = postSelected.querySelector(".title p").innerText.trim();
+    var content = postSelected.querySelector(".content").innerText.trim();
+
+    sessionStorage.setItem('author',author);
+    sessionStorage.setItem('title',title);
+    sessionStorage.setItem('content',content);
+    
+    window.location.href='../html/post.html'
+}
+
+//Delete the Post
+deletePost = () =>{
+    postSelected.remove();
+}
+
 //Get the Parent Div for the selected post
-function getPostData(e){
+getPostData = (e) =>{
     if(e.target.classList.contains('fa-trash')){
         postSelected = (e.target.parentElement.parentElement.parentElement.parentElement);
     }
+
+    if(e.target.classList.contains('fa-ellipsis-h')){
+        postSelected = (e.target.parentElement.parentElement.parentElement);
+        test();
+    }
 }
 
-//Bind click event listner
+//Event Listener
 document.getElementById("postContainer").addEventListener("click",getPostData);
-
-//Delete the Post
-function deletePost(){
-    postSelected.remove();
-}
