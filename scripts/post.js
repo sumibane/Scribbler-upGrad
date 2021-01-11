f8eac82b393a6e7669360011312b7751b08cec4f
Sumit Banerjee <sum123.ban@gmail.com>
2021-01-11 17:18:38 +0530

    Comments added

--- scripts/post.js
+++ scripts/post.js
@@ -1,28 +1,28 @@
 //To load the contents of the post during page loading.
 window.onload = () => {
     var title = document.getElementById('title');
-    title.innerHTML = '<p class="editable">'+sessionStorage.getItem('title')+'</p>';
+    title.innerHTML = '<p class="editable">' + sessionStorage.getItem('title') + '</p>';
     var author = document.getElementById('author');
-    author.innerHTML = '<p class="editable">'+sessionStorage.getItem('author')+'</p>'
+    author.innerHTML = '<p class="editable">' + sessionStorage.getItem('author') + '</p>'
     var content = document.getElementById('contents');
-    content.innerHTML = '<p class="editable">'+sessionStorage.getItem('content')+'</p>'
-}
+    content.innerHTML = '<p class="editable">' + sessionStorage.getItem('content') + '</p>'
+};
 
 //Click event Listener to handle the Edit & Save functionality
 document.addEventListener('click', (e) => {
-    if(e.target.id === 'btn-edit'){
+    if (e.target.id === 'btn-edit') {
         var btn = document.getElementById('btn-edit');
         //When the Edit Button is pressed.
-        if(btn.innerText.trim() === 'Edit'){
+        if (btn.innerText.trim() === 'Edit') {
             btn.innerHTML = 'Save <i class="fas fa-save"></i>';
             var editableContents = document.querySelectorAll('.editable');
             Array.from(editableContents).forEach((editableContent, index) => {
-            editableContent.classList.add('editEnabled');
-            editableContent.setAttribute('contenteditable', true);
-        });
+                editableContent.classList.add('editEnabled');
+                editableContent.setAttribute('contenteditable', true);
+            });
         }
         //When the save Button is pressed.
-        else if(btn.innerText.trim() === 'Save'){
+        else if (btn.innerText.trim() === 'Save') {
             btn.innerHTML = 'Edit <i class="fas fa-edit"></i>';
             var editableContents = document.querySelectorAll('.editable');
             Array.from(editableContents).forEach((editableContent, index) => {
@@ -30,18 +30,37 @@ document.addEventListener('click', (e) => {
                 editableContent.setAttribute('contenteditable', false);
             });
         }
-    }    
-})
+    }
+});
 
-//Like Functionality Handler
 var likeCount = 0;
-document.getElementById('btn-like').addEventListener('click', (e) =>{
+//Click Event Listener for manging the Like functionality
+document.getElementById('btn-like').addEventListener('click', (e) => {
     e.target.innerHTML = '<i class="fas fa-thumbs-up"></i> Liked';
     likeCount++;
-    if(likeCount == 1){
+    if (likeCount == 1) {
         document.querySelector('.likeSec .likeCount').innerHTML = '1 person likes this!';
     }
-    else{
-        document.querySelector('.likeSec .likeCount').innerHTML = likeCount+' people like this!';
+    else {
+        document.querySelector('.likeSec .likeCount').innerHTML = likeCount + ' people like this!';
     }
-})
+});
+
+//Click event for adding the comments
+document.getElementById('postComment').addEventListener('click', (e) => {
+    if (e.target.id == 'btn-comment') {
+        var commentText = document.getElementById('txt-comment').value;
+        var commentBox = document.querySelector('.viewComment .comments');
+        if (commentText.trim() != '') {
+            if (commentBox.style.display = "none") {
+                commentBox.style.display = "block";
+            }
+            commentBox.innerHTML = '<div>' + commentText + '</div>' + commentBox.innerHTML;
+            document.getElementById('txt-comment').value = '';
+        }
+        else {
+            alert("Comment cannot be empty");
+            document.getElementById('txt-comment').value = '';
+        }
+    }
+});
--- scripts/postslist.js
+++ scripts/postslist.js
@@ -6,29 +6,29 @@ redirectPost = () => {
     var title = postSelected.querySelector(".title p").innerText.trim();
     var content = postSelected.querySelector(".content").innerText.trim();
 
-    sessionStorage.setItem('author',author);
-    sessionStorage.setItem('title',title);
-    sessionStorage.setItem('content',content);
-    
-    window.location.href='../html/post.html'
-}
+    sessionStorage.setItem('author', author);
+    sessionStorage.setItem('title', title);
+    sessionStorage.setItem('content', content);
+
+    window.location.href = '../html/post.html'
+};
 
 //Delete the Post
-deletePost = () =>{
+deletePost = () => {
     postSelected.remove();
-}
+};
 
 //Get the Parent Div for the selected post
-getPostData = (e) =>{
-    if(e.target.classList.contains('fa-trash')){
+getPostData = (e) => {
+    if (e.target.classList.contains('fa-trash')) {
         postSelected = (e.target.parentElement.parentElement.parentElement.parentElement);
     }
 
-    if(e.target.classList.contains('fa-ellipsis-h')){
+    if (e.target.classList.contains('fa-ellipsis-h')) {
         postSelected = (e.target.parentElement.parentElement.parentElement);
         redirectPost();
     }
-}
+};
 
 //Event Listener
-document.getElementById("postContainer").addEventListener("click",getPostData);
+document.getElementById("postContainer").addEventListener("click", getPostData);
